094653dff77ab8d968f099d4f71e99e49feda82e
guilhermey <magalheasinove@gmail.com>
2024-08-29 20:46:16 -0400

    update

--- usuarios/views.py
+++ usuarios/views.py
@@ -40,4 +40,28 @@ def cadastro(request):
         
 
 def home(request):
-    return render(request, 'usuarios/home.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/home.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+def lancar(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/lancar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+def alterar(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/alterar.html')
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+def visualizar(request):
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/visualizar.html' )
+    else:
+        return HttpResponse("Faça o login para acessar!")
+
+
+
